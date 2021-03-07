295b7345af8bf819bec54b9bae83a7d8e9e8a994
Ewelina Wesołowska <ewelwesolowskae@gmail.com>
2021-03-07 16:44:16 +0100

    Initial commit

---
+++ mysite/polls/settings_local.py
@@ -0,0 +1,12 @@
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dzikie_kaczki',
+        'USER': 'dzikie_kaczki',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
